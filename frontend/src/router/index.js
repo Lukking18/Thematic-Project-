c52dcb1e97b0eeddb569398a1902bd836fb72fc7
souley-3131 <souleycoul3131@gmail.com>
2026-03-19 15:12:43 +0000

    login/logout, not yet properly working

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginPage from "../Login.vue";
 import HomePage from "../Home.vue";
+import SignUpPage from "../SignUp.vue";
 
 const routes = [
-  { path: "/", component: HomePage, meta: { requiresAuth: true } },
-  { path: "/login", component: LoginPage }
+  { path: "/", component: HomePage, meta: { requiresAuth: true }
+},
+  { path: "/login", component: LoginPage },
+  {path: "/signup", component: SignUpPage}
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem("token");
+  if (to.meta.requiresAuth && !token) {
+    return next("/login");
+  }
+  next();
 });
 
 export default router;
